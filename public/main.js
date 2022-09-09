0bf5fc8b71bebd60a109cb822eb06160bad4fad8
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-09-09 11:17:38 -0700

    cleaned up code

--- public/main.js
+++ public/main.js
@@ -3,77 +3,79 @@ let byeBtn = document.getElementsByClassName("bye")
 let submitBtnReload = document.querySelector('form')
 let containerSection = document.getElementsByClassName('container')
 
-submitBtnReload.addEventListener('submit', function (){
+submitBtnReload.addEventListener('submit', function () {
     window.location.reload(true)
 })
 
-Array.from(byeBtn).forEach(function (element){
-    element.addEventListener('click', function (event){
+Array.from(byeBtn).forEach(function (element) {
+    element.addEventListener('click', function (event) {
         const destinationName = event.target.parentNode.children[0].innerText
         const locationName = event.target.parentNode.children[1].innerText
         console.log("LOC IS", locationName)
         console.log("DEST IS", destinationName);
 
-       fetch('/remove', {
-            method: 'delete', 
+        fetch('/remove', {
+            method: 'delete',
             headers: {
                 'Content-Type': 'application/json'
-            }, 
-            body:JSON.stringify({
-                locationName: locationName, 
+            },
+            body: JSON.stringify({
+                locationName: locationName,
                 destinationName: destinationName
             })
         })
-        .then(function (res){
-            window.location.reload(true)
-        })
-        .catch(err=>{
-            console.log(err);
-        })
+            .then(function (res) {
+                window.location.reload(true)
+            })
+            .catch(err => {
+                console.log(err);
+            })
     })
 
 
 })
 
-Array.from(editBtn).forEach(function (element){
-    element.addEventListener('click', async function (){
+Array.from(editBtn).forEach(function (element) {
+    element.addEventListener('click', async function () {
         const oldLocationName = document.getElementById('locationName').innerText
         let editedDest = window.prompt('New destination name:')
         let editedLoc = window.prompt('New location name:')
         let editedNotes = window.prompt('New notes:')
-        // console.log("loc, des", locationName, destinationName);
-
-       try{
-        await fetch('/editText', {
-            method: 'put', 
-            headers: {
-                'Content-Type': 'application/json'
-            }, 
-            body:JSON.stringify({
-                newLocationName: editedLoc, 
-                newDestinationName: editedDest, 
-                newNotes: editedNotes, 
-                oldLocationName: oldLocationName
-            })
-        })
-        //with async await, all of the code waits, 
-        //whereas with .then, it doesnt
-    }catch(err){
-            console.log(err);
-    }
-        try{
+        if (editedDest !== "" && editedLoc !== "") {
+            try {
+                await fetch('/editText', {
+                    method: 'put',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        newLocationName: editedLoc,
+                        newDestinationName: editedDest,
+                        newNotes: editedNotes,
+                        oldLocationName: oldLocationName
+                    })
+                })
+                //with async await, all of the code waits, 
+                //whereas with .then, it doesnt
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            window.alert('Sorry, the location and destination fields cannot be empty -- please try again!')
+        }
+        try {
             await fetch('/newImage', {
-            method: 'PUT', 
-            headers: {
-                'Content-Type': 'application/json'
-            }, 
-            body:JSON.stringify({
-                newLocationName: editedLoc, 
-                newDestinationName: editedDest, 
-            })  
-        })        
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    newLocationName: editedLoc,
+                    newDestinationName: editedDest,
+                })
+            })
             window.location.reload(true)
-    }catch(err){
+        } catch (err) {
             console.log(err);
         }
     })
@@ -84,4 +86,3 @@ Array.from(editBtn).forEach(function (element){
 // https://javascript.info/async
 //https://javascript.info/network
 
-        
--- server.js
+++ server.js
@@ -1,103 +1,101 @@
 const env = require('dotenv').config()
 const path = require('path')
-const { log } = require('console')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://djt:d12345678@cluster0.8ydmb5x.mongodb.net/vacay?retryWrites=true&w=majority'
 express.static('public')
-let randomNum = Math.random()*10
 const fetch = require('node-fetch')
-const { read } = require('fs')
 
 MongoClient.connect(connectionString, {
     useUnifiedTopology: true
 }).catch(err => console.log(err))
-.then(client=>{
-    console.log("connected to DB");
-    const db= client.db('vacay')
-    const vacayCollection = db.collection('vacay')
-    app.set('views', path.join(__dirname, 'public'))
-    app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.use(bodyParser.json())
-    app.use(express.static('public'))
-    app.listen(process.env.PORT || 3000, function(){
-        console.log("listening on 3000");
-    })
-    app.get('/', function (req, res){
-        vacayCollection.find().toArray()
-        .then(data=>{
-            res.render('index.ejs', {imgData: data})
+    .then(client => {
+        console.log("connected to DB");
+        const db = client.db('vacay')
+        const vacayCollection = db.collection('vacay')
+        app.set('views', path.join(__dirname, 'public'))
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
+        app.listen(process.env.PORT || 3000, function () {
+            console.log("listening on 3000");
         })
-    })
-    
-    app.post('/grabImage', async function (req, res){
-        try{
-            let searchTerm = req.body.destination + " " + req.body.location
-            const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.accessKey}&query=${searchTerm}&limit=5`
-            let response = await fetch(url)
-            let responseText = await response.json()
-            await vacayCollection.insert({
-                dest: req.body.destination.trim(), 
-                loc: req.body.location.trim(), 
-                responseText
-            })
-            res.redirect('/')
-        }catch(err){
-            console.log(err);
-        }
+        app.get('/', function (req, res) {
+            vacayCollection.find().toArray()
+                .then(data => {
+                    res.render('index.ejs', { imgData: data })
+                })
         })
-    app.delete('/remove', async function (req, res){
-            try{
+
+        app.post('/grabImage', async function (req, res) {
+            try {
+                let searchTerm = req.body.destination + " " + req.body.location
+                const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.accessKey}&query=${searchTerm}&limit=5`
+                let response = await fetch(url)
+                let responseText = await response.json()
+                await vacayCollection.insert({
+                    dest: req.body.destination.trim(),
+                    loc: req.body.location.trim(),
+                    responseText
+                })
+                res.redirect('/')
+            } catch (err) {
+                console.log(err);
+            }
+        })
+        app.delete('/remove', async function (req, res) {
+            try {
                 await vacayCollection.deleteOne(
-                {dest: req.body.destinationName}, 
-            )                
-            res.redirect('/')
-        }catch(err){
-            console.log(err);
-        }
-    })
-    app.put('/editText', async function (req, res){
-        console.log("editText", req.body);
-        try{
-            const result = await vacayCollection.findOneAndUpdate( 
-                {loc: req.body.oldLocationName}, 
-                {
-                    $set:{
-                        loc: req.body.newLocationName, 
-                        dest: req.body.newDestinationName,                     }
-                }
-            )
+                    { dest: req.body.destinationName },
+                )
+                res.redirect('/')
+            } catch (err) {
+                console.log(err);
+            }
+        })
+        app.put('/editText', async function (req, res) {
+            console.log("editText", req.body);
+            try {
+                const result = await vacayCollection.findOneAndUpdate(
+                    { loc: req.body.oldLocationName },
+                    {
+                        $set: {
+                            loc: req.body.newLocationName,
+                            dest: req.body.newDestinationName,
+                        }
+                    }
+                )
                 console.log("RESULT:", result)
                 res.redirect('/')
-        }catch(err){
-            console.log("err:", err);
-        }
-    })
-        
-    app.put('/newImage', async function (req, res){
-        try{
-            let searchTerm = req.body.newDestinationName + " " + req.body.newLocationName
-            const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.accessKey}&query=${searchTerm}&limit=5`
-            let response = await fetch(url)
-            let newResponseText = await response.json()
-            await vacayCollection.findOneAndUpdate(
-                {loc: req.body.newLocationName}, 
-                {
-                    $set:{
-                        responseText: newResponseText
-                    }
-                } 
-            )
-            res.redirect('/')
-        }catch(err){
-            console.log(err);
-        }
-
-        
+            } catch (err) {
+                console.log("err:", err);
+            }
         })
-})
+
+        app.put('/newImage', async function (req, res) {
+            try {
+                let searchTerm = req.body.newDestinationName + " " + req.body.newLocationName
+                const url = `https://api.unsplash.com/search/photos/?client_id=${process.env.accessKey}&query=${searchTerm}&limit=5`
+                let response = await fetch(url)
+                let newResponseText = await response.json()
+                await vacayCollection.findOneAndUpdate(
+                    { loc: req.body.newLocationName },
+                    {
+                        $set: {
+                            responseText: newResponseText
+                        }
+                    }
+                )
+                res.redirect('/')
+            } catch (err) {
+                console.log(err);
+            }
+
+
+        })
+    })
 
 
